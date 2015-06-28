eae35f8755e33fbeb43736a9154d0044402e3eba
Alessandro Bellucci <alessandro.bellucci@3e.eu>
2015-06-28 20:51:04 +0200

    Fixed api url

--- denv-core/src/main/java/org/ssoup/denv/core/api/DenvApiEndpoints.java
+++ denv-core/src/main/java/org/ssoup/denv/core/api/DenvApiEndpoints.java
@@ -10,10 +10,10 @@ public class DenvApiEndpoints {
     public static final String ENVS = "/environments";
     public static final String ENV_CONFIGS = "/environmentConfigs";
     public static final String ENV_CONFIG_VERSIONS = "/environmentConfigs/{envConfId}/versions";
+    public static final String TEST_RESULTS = "/environmentConfigs/{envConfId}/versions/{version}/testResults";
+    public static final String GET_LATEST_STABLE_VERSION = "/environmentConfigs/{envConfId}/getLatestStableVersion";
 
     // relative
     public static final String ENV_SAVE_SNAPSHOT = "/{envId}/saveSnapshot";
     public static final String ENV_CONFIG_VERSION = "/{version:.+}";
-    public static final String TEST_RESULTS = "/{envConfId}/{version}/testResults";
-    public static final String GET_LATEST_STABLE_VERSION = "/{envConfId}/getLatestStableVersion";
 }
